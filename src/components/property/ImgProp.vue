ffa7856194d4315c8eb629c572f04b0323e132f6
qianmin <1075111314@qq.com>
2022-08-22 23:49:07 +0800

    对接后端上传接口

--- src/components/property/ImgProp.vue
+++ src/components/property/ImgProp.vue
@@ -1,25 +1,23 @@
 <template>
   <div>
-    <div style="display: flex;margin-bottom: 5%;">
+    <div class='module'>常规</div>
+    <div style="display: flex;align-items: center;margin-top: 2%;">
       <div class="label">代替文本：</div>
       <el-input class="input" v-model="views.alt"></el-input>
     </div>
-    <div style="display: flex;margin-bottom: 5%;">
+    <div style="display: flex;align-items: center;margin-top: 5%;">
       <div class="label">图片路径：</div>
       <el-input class="input" v-model="views.src"></el-input>
     </div>
     <!-- 下面代码未定义的函数被我删了，不然会有bug -->
-    <el-upload
-      class="upload-demo"
-      action="https://jsonplaceholder.typicode.com/posts/"
-      multiple
-    >
-      <el-button size="small" type="primary">点击上传</el-button>
-    </el-upload>
+    <el-upload action="upload" :on-success="success" :show-file-list="false" :before-upload="beforeUpload" style="margin-top:5%">
+      <el-button size="small" type="primary" >点击上传</el-button>
+  </el-upload>
   </div>
 </template>
 
 <script>
+import { Message } from 'element-ui';
 export default {
     props:["views"],
     data() {
@@ -27,6 +25,22 @@ export default {
             
         }
     },
+    methods:{
+       success(response) {
+      this.views.src = 'http://127.0.0.1:3001/' + response.path.slice(7)
+    },
+    beforeUpload(file) {
+      const isSvg = file.type === 'img/svg';
+      const isLt30M = file.size / 1024 / 1024 < 30;
+      if (isSvg) {
+        Message.error('上传图片不能是 svg 格式!');
+      }
+      if (!isLt30M) {
+        Message.error('上传图片大小不能超过 30MB!');
+      }
+      return isSvg && isLt30M;
+    },
+    }
 };
 </script>
 
@@ -43,6 +57,18 @@ export default {
   display: flex;
   justify-content: left;
   align-items: center;
+}
+.module {
+  width: 280px;
+  margin-top: 3px;
+  padding-bottom: 5px;
+  display: flex;
+  align-items: center;
+  background-color: rgb(219, 226, 242);
+  text-align: left;
+  font-weight: 700;
+  text-indent: 1em;
+  line-height: 36px
 }
 
 </style>
